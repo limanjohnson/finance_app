56e131a80b31fb820bcda7da0030fd3a6beb628f
Liam <liamjnell@gmail.com>
2025-11-11 22:49:03 -0700

    Create Finance Application

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Welcome to the Finance Tracker");
+    println!("\nThere are many awesome features in the works. Stay tuned!");
+
+    // List of things to develop:
+    // 1. User Authentication
+    // 2. Income tracking
+    // 3. Expense tracking
+    // 4. Budget planning
+    // 5. Financial reports generation
+    // 6. Data visualization
+    // 7. Transaction categorization
+    // 8. Exporting data to CSV/Excel
+}
